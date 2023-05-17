e87eec5d139ee47eb7e1e80b8d972fff3c35be88
Perkles <helena999end@gmail.com>
2023-05-17 17:25:15 -0300

    atualizacao

--- EstoqueDeBriquedos1.0/src/Estoque/Brinquedo.java
+++ EstoqueDeBriquedos1.0/src/Estoque/Brinquedo.java
@@ -1,48 +1,53 @@
 package Estoque;
 
-public abstract class Brinquedo  {
-	
-    private int codigo;
-    private Fornecedor fornecedor;
-    private double custo;
-    
+public abstract class Brinquedo {
+
+	private int codigo;
+	private Fornecedor fornecedor;
+	private double custo;
+
 	public Brinquedo() {
 		this.codigo = 0;
 		this.custo = 0;
-	    this.fornecedor = new Fornecedor();
+		this.fornecedor = new Fornecedor();
 	}
-	
+
 	public Brinquedo(int codigo, Fornecedor fornecedor, double custo) {
 		this.codigo = codigo;
 		this.custo = custo;
-	    this.fornecedor = new Fornecedor();
-	    
+		this.fornecedor = new Fornecedor();
+
 	}
-	
+
 	public abstract double calculaCusto();
-	
-	
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public Fornecedor getFornecedor() {
 		return fornecedor;
 	}
+
 	public void setFornecedor(Fornecedor fornecedor) {
 		this.fornecedor = fornecedor;
 	}
+
 	public double getCusto() {
 		return custo;
 	}
+
 	public void setCusto(double custo) {
 		this.custo = custo;
 	}
+
 	@Override
 	public String toString() {
 		return "Brinquedo [Código= " + codigo + ", Fornecedor= " + fornecedor + ", Custo= " + custo + "]";
 	}
-    
+
 }
--- EstoqueDeBriquedos1.0/src/Estoque/Carro.java
+++ EstoqueDeBriquedos1.0/src/Estoque/Carro.java
@@ -1,45 +1,39 @@
 package Estoque;
 
-public class Carro extends Brinquedo{
+public class Carro extends Brinquedo {
 
 	private double precoUnitarioCarro;
 	private String modelo;
-	
+
 	public Carro() {
 		super();
 		this.precoUnitarioCarro = 0;
 		this.modelo = "-";
-		
+
 	}
-	
-	public Carro(int codigo, Fornecedor fornecedor, double custo, double precoUnitarioCarro,String modelo) {
+
+	public Carro(int codigo, Fornecedor fornecedor, double custo, double precoUnitarioCarro, String modelo) {
 		super(codigo, fornecedor, custo);
 		this.precoUnitarioCarro = precoUnitarioCarro;
 		this.modelo = modelo;
 	}
 
-
-
 	public double calculaCusto() {
-		
+
 		return getPrecoUnitarioCarro() + getCusto();
 	}
-
 
 	public double getPrecoUnitarioCarro() {
 		return precoUnitarioCarro;
 	}
 
-
 	public void setPrecoUnitarioCarro(double precoUnitarioCarro) {
 		this.precoUnitarioCarro = precoUnitarioCarro;
 	}
 
-
 	public String getModelo() {
 		return modelo;
 	}
-
 
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
@@ -49,6 +43,5 @@ public class Carro extends Brinquedo{
 	public String toString() {
 		return "Carro -> Preço Unitário= " + precoUnitarioCarro + ", Modelo do carro= " + modelo;
 	}
-	
 
 }
